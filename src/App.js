5fdd6a5eb1ea0e74b8efddf49973a0fdebead918
Sanjeet Jha <sanjeetk77@gmail.com>
2024-11-01 20:24:36 +0530

    react pagination source code

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import logo from './logo.svg';
+import {useState, useMemo} from 'react';
+import './App.css';
+import data from './data/mock-data.json'
+import Pagination from './Pagination';
+
+let pageSize = 20;
+
+
+function App() {
+  const [currentPage, setcurrentPage] = useState(1);
+
+  const currentTableData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * pageSize;
+    const lastPageIndex = firstPageIndex + pageSize;
+    return data.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage]);
+
+
+  return (
+    <>
+    <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>FIRST NAME</th>
+            <th>LAST NAME</th>
+            <th>EMAIL</th>
+            <th>PHONE</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentTableData.map(item => {
+            return (
+              <tr>
+                <td>{item.id}</td>
+                <td>{item.first_name}</td>
+                <td>{item.last_name}</td>
+                <td>{item.email}</td>
+                <td>{item.phone}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <Pagination
+        className = 'pagination-bar'
+        currentPage={currentPage}
+        totalCount = {data.length}
+        pageSize={pageSize}
+        onPageChange = {page => setcurrentPage(page)}
+      />
+    
+    </>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
